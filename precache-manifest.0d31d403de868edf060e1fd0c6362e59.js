f6b9918ecd6f3f613c687c0fa8e8df7c963e8bb7
Akash Verma <akashvermaofsjs@gmail.com>
2020-05-24 15:58:36 +0530

    Updates

--- precache-manifest.0d31d403de868edf060e1fd0c6362e59.js
+++ precache-manifest.0d31d403de868edf060e1fd0c6362e59.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "388f94d11a2b62c9fca4553ff6531844",
+    "revision": "b8f033ff95b7754d64f17a47e302e1e2",
     "url": "/my-portfolio-app/index.html"
   },
   {
-    "revision": "9f74ed12b5ffb2a08124",
+    "revision": "91729189ead7e0e2240d",
     "url": "/my-portfolio-app/static/css/main.1346168d.chunk.css"
   },
   {
-    "revision": "beef512ce277d4abd653",
-    "url": "/my-portfolio-app/static/js/2.4245746c.chunk.js"
+    "revision": "1d092f37d98d1759e6cb",
+    "url": "/my-portfolio-app/static/js/2.6c461b6f.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/my-portfolio-app/static/js/2.4245746c.chunk.js.LICENSE.txt"
+    "url": "/my-portfolio-app/static/js/2.6c461b6f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f74ed12b5ffb2a08124",
-    "url": "/my-portfolio-app/static/js/main.9f79ec22.chunk.js"
+    "revision": "91729189ead7e0e2240d",
+    "url": "/my-portfolio-app/static/js/main.2fe8ae31.chunk.js"
   },
   {
     "revision": "47f8e045759f605913fc",
