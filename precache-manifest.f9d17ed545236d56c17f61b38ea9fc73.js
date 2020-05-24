a73e7e7e67cb4cab3f26cd7ac76bc5d5d5b5e343
Akash Verma <akashvermaofsjs@gmail.com>
2020-05-24 15:11:13 +0530

    Updates

---
+++ precache-manifest.f9d17ed545236d56c17f61b38ea9fc73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27414562c7d510d7c54148fb8adace64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03189829ffab6391f8a0",
+    "url": "/static/css/main.1346168d.chunk.css"
+  },
+  {
+    "revision": "beef512ce277d4abd653",
+    "url": "/static/js/2.4245746c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4245746c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03189829ffab6391f8a0",
+    "url": "/static/js/main.ceecd274.chunk.js"
+  },
+  {
+    "revision": "17553778e02af952c207",
+    "url": "/static/js/runtime-main.455471ad.js"
+  }
+]);
