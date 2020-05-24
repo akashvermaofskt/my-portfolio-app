0b1e7ccad3b0e70fbd1dfe64dd75ea2b66a7d10d
Akash Verma <akashvermaofsjs@gmail.com>
2020-05-24 15:37:11 +0530

    Added build files

---
+++ build/precache-manifest.e20918b705e590099ba7c11863a36f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388f94d11a2b62c9fca4553ff6531844",
+    "url": "/my-portfolio-app/index.html"
+  },
+  {
+    "revision": "9f74ed12b5ffb2a08124",
+    "url": "/my-portfolio-app/static/css/main.1346168d.chunk.css"
+  },
+  {
+    "revision": "beef512ce277d4abd653",
+    "url": "/my-portfolio-app/static/js/2.4245746c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-portfolio-app/static/js/2.4245746c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f74ed12b5ffb2a08124",
+    "url": "/my-portfolio-app/static/js/main.9f79ec22.chunk.js"
+  },
+  {
+    "revision": "47f8e045759f605913fc",
+    "url": "/my-portfolio-app/static/js/runtime-main.2fce0bed.js"
+  }
+]);
